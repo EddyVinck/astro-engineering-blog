a88ef405bd76b155e2c73a04b1982467605254f0
eddyvinck <eddy@frontvalue.nl>
2023-04-18 13:53:59 +0200

    change constant variables for site and brand names

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const BRAND = "Acme";
+export const SITE_TITLE = "Acme Engineering";
+export const SITE_DESCRIPTION = "Welcome to the Acme Engineering blog!";
