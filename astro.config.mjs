cc9977d20b9d2b262e4317e0115a3c6323a9e1eb
eddyvinck <eddy@frontvalue.nl>
2023-04-21 10:17:31 +0200

    convert header to tailwind css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import solidJs from "@astrojs/solid-js";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), solidJs()]
+  integrations: [mdx(), sitemap(), solidJs(), tailwind()]
 });
