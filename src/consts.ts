28f5f044efbe01409b302f55a7c83a6aa8c3e56f
eddyvinck <eddy@frontvalue.nl>
2023-04-24 17:15:49 +0200

    add image to homepage

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const BRAND = "Acme";
-export const SITE_TITLE = "Acme Engineering";
+export const SITE_TITLE = "Astro Engineering Blog";
 export const SITE_URL = "https://astro-engineering-blog.netlify.app";
-export const SITE_DESCRIPTION = "Welcome to the Acme Engineering blog!";
+export const SITE_DESCRIPTION = "Welcome to our Engineering blog!";
 export const PAGINATION_POSTS_PER_PAGE = 5;
