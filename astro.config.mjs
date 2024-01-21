c5b00ffd6f87033555a8a6b676efff1e94feddf3
Eddy Vinck <eddyvinck95@gmail.com>
2024-01-21 22:20:33 +0100

    try netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,14 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
-import nodejs from "@astrojs/node";
+import netlify from "@astrojs/netlify";
 import { SITE_URL } from "./src/consts.ts";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
-  adapter: nodejs({
-    mode: "middleware",
+  output: "",
+  adapter: netlify({
+    edgeMiddleware: false,
   }),
   site: SITE_URL,
   integrations: [
