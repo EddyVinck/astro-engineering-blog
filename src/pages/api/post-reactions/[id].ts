382f6fba05496d5b688079f7f2ca943b9c642323
Eddy Vinck <eddyvinck95@gmail.com>
2024-01-29 00:43:50 -0600

    remove rate limit for now

--- src/pages/api/post-reactions/[id].ts
+++ src/pages/api/post-reactions/[id].ts
@@ -5,25 +5,11 @@ import {
   type PostReactionOption,
   type PostReactions,
 } from "../../../lib/appwrite/appwrite.server";
-import rateLimit, { type RateLimitHeaders } from "../../../lib/ratelimit";
 
 export const prerender = false;
 
-const Getlimiter = rateLimit({
-  interval: 10 * 1000, // 10 seconds
-  uniqueTokenPerInterval: 500, // Max 500 users per second
-});
-
-const Postlimiter = rateLimit({
-  interval: 10 * 1000, // 10 seconds
-  uniqueTokenPerInterval: 500, // Max 500 users per second
-});
-
-export const GET: APIRoute = async ({ params, request }): Promise<Response> => {
+export const GET: APIRoute = async ({ params }): Promise<Response> => {
   const id = params.id;
-  // This is a Netlify header. Astro has client address but it doesn't work in the Netlify adapter.
-  const userIP = request.headers.get("client-ip");
-  console.log("user ip", userIP);
 
   if (!import.meta.env.SECRET_APPWRITE_API_KEY) {
     return new Response(JSON.stringify({ error: "internal server error" }), {
@@ -31,45 +17,10 @@ export const GET: APIRoute = async ({ params, request }): Promise<Response> => {
     });
   }
 
-  // #region ratelimit
-  let rateLimitHeaders: RateLimitHeaders | null = null;
-  try {
-    rateLimitHeaders = await Getlimiter.check(25, `GET-REACTIONS-${userIP}`);
-  } catch (error: any) {
-    if ("X-RateLimit-Limit" in error && "X-RateLimit-Remaining" in error) {
-      const err = error as RateLimitHeaders;
-      rateLimitHeaders = err;
-      return new Response(JSON.stringify({ error: "Rate limit exceeded" }), {
-        status: 429,
-        headers: new Headers({
-          "X-RateLimit-Limit": err["X-RateLimit-Limit"].toString(),
-          "X-RateLimit-Remaining": err["X-RateLimit-Remaining"].toString(),
-        }),
-      });
-    }
-  }
-  if (
-    typeof rateLimitHeaders?.["X-RateLimit-Limit"] === "undefined" ||
-    typeof rateLimitHeaders?.["X-RateLimit-Remaining"] === "undefined"
-  ) {
-    console.log("⚠️ Did not find rate limit headers");
-    return new Response(JSON.stringify({ error: "internal server error" }), {
-      status: 500,
-    });
-  }
-
-  const headers = new Headers({
-    "X-RateLimit-Limit": rateLimitHeaders["X-RateLimit-Limit"].toString(),
-    "X-RateLimit-Remaining":
-      rateLimitHeaders["X-RateLimit-Remaining"].toString(),
-  });
-  // #endregion
-
   if (!id) {
     return new Response(JSON.stringify({ error: "not found" }), {
       status: 404,
       statusText: "Not found",
-      headers: headers,
     });
   }
 
@@ -92,18 +43,13 @@ export const GET: APIRoute = async ({ params, request }): Promise<Response> => {
     poops: postReactions.poops,
   };
 
-  headers.append("Content-Type", "application/json");
-
   return new Response(JSON.stringify(postReactionData), {
     status: 200,
-    headers: headers,
   });
 };
 
 export const POST: APIRoute = async ({ request, params }) => {
   const id = params.id;
-  // This is a Netlify header. Astro has client address but it doesn't work in the Netlify adapter.
-  const userIP = request.headers.get("client-ip");
 
   if (!import.meta.env.SECRET_APPWRITE_API_KEY) {
     return new Response(JSON.stringify({ error: "internal server error" }), {
@@ -111,46 +57,10 @@ export const POST: APIRoute = async ({ request, params }) => {
     });
   }
 
-  // #region ratelimit
-  let rateLimitHeaders: RateLimitHeaders | null = null;
-  try {
-    rateLimitHeaders = await Postlimiter.check(10, `POST-REACTIONS-${userIP}`);
-  } catch (error: any) {
-    if ("X-RateLimit-Limit" in error && "X-RateLimit-Remaining" in error) {
-      const err = error as RateLimitHeaders;
-      rateLimitHeaders = err;
-      return new Response(JSON.stringify({ error: "Rate limit exceeded" }), {
-        status: 429,
-        headers: new Headers({
-          "X-RateLimit-Limit": err["X-RateLimit-Limit"].toString(),
-          "X-RateLimit-Remaining": err["X-RateLimit-Remaining"].toString(),
-        }),
-      });
-    }
-  }
-
-  if (
-    typeof rateLimitHeaders?.["X-RateLimit-Limit"] === "undefined" ||
-    typeof rateLimitHeaders?.["X-RateLimit-Remaining"] === "undefined"
-  ) {
-    console.log("⚠️ Did not find rate limit headers");
-    return new Response(JSON.stringify({ error: "internal server error" }), {
-      status: 500,
-    });
-  }
-
-  const headers = new Headers({
-    "X-RateLimit-Limit": rateLimitHeaders["X-RateLimit-Limit"].toString(),
-    "X-RateLimit-Remaining":
-      rateLimitHeaders["X-RateLimit-Remaining"].toString(),
-  });
-  // #endregion
-
   if (!id) {
     return new Response(JSON.stringify({ error: "not found" }), {
       status: 404,
       statusText: "Not found",
-      headers: headers,
     });
   }
 
@@ -171,7 +81,6 @@ export const POST: APIRoute = async ({ request, params }) => {
 
       return new Response(JSON.stringify(newReactions), {
         status: 200,
-        headers: headers,
       });
     } catch (error) {
       console.log(`🚨 err when reacting to ID "${id}"!`, error);
