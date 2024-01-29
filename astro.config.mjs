9c11f37c7dba58908da735f896c5fef2086b4145
Eddy Vinck <eddyvinck95@gmail.com>
2024-01-29 01:32:47 -0600

    retry with netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,16 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import solidJs from "@astrojs/solid-js";
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify";
 import { SITE_URL } from "./src/consts";
-
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: "hybrid",
-  adapter: vercel(),
+  adapter: netlify({
+    edgeMiddleware: false,
+    imageCDN: false, // Blog hero image doesn't seem to work with this enabled
+  }),
   site: SITE_URL,
   integrations: [
     mdx(),
